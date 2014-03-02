9227a2ea6627659bbf5b2e0c7c620ec775a978d9
Mike Dory <mdory@kbsp.com>
2014-03-02 18:33:06 -0500

    adding all the class data

--- classData.js
+++ classData.js
@@ -6,10 +6,33 @@
 
 // ========
 module.exports = {
-  bodyCopy: function () {
-    // whatever
-  },
-  classCopy: function () {
-    // whatever
+  main: function () {
+
+    // fill out the copy for the page
+    var text = {
+      pageTitle: 'DWD at ITP',
+      pageDescription: 'Dynamic Web Development (Server) &mdash; Spring 2014',
+      sidebarTitle: 'DWD @ ITP',
+      classDescription: '',
+      instructors: [
+        {firstName: "Cory", lastName: "Forsyth", email: ''},
+        {firstName: "Mike", lastName: "Dory", email: 'mike@dory.me'},
+      ],
+      classes: [
+        {
+          title: "Class 1 - Intro",
+          description: "BOOOO",
+          links: [
+            {
+              title: "Gettings Started with Node.js on Heroku",
+              href: "https://devcenter.heroku.com/articles/getting-started-with-nodejs"
+            }
+          ]
+        }
+      ]
+    };
+
+    return text;
+
   }
 };
