b9a6cfc7e9197e166af0bf38857dfee55ed0fcf7
Mike Dory <mdory@kbsp.com>
2014-03-02 18:32:55 -0500

    moving bodycopy into its own module

--- app.js
+++ app.js
@@ -26,33 +26,12 @@ app.use(express.logger('dev'));
 // define env variables
 app.set('port', process.env.PORT || 3000);
 
-// fill out the copy for the page
-var bodyCopy = {
-  pageTitle: 'DWD at ITP',
-  pageDescription: 'Dynamic Web Development (Server) &mdash; Spring 2014',
-  sidebarTitle: 'DWD @ ITP',
-  instructors: [
-    {firstName: "Cory", lastName: "Forsyth", email: ''},
-    {firstName: "Mike", lastName: "Dory", email: 'mike@dory.me'},
-  ],
-  classes: [
-    {
-      name: "Class 1 - Intro",
-      links: [
-        {
-          title: "Gettings Started with Node.js on Heroku",
-          href: "https://devcenter.heroku.com/articles/getting-started-with-nodejs"
-        }
-      ]
-    }
-  ]
-};
-
 // define us some routes
 app.get('/', function(req, res) {
-  res.render('index', bodyCopy);
+  res.render('index', classData.main());
 });
 
 // ready, set, rocket
 app.listen(app.get('port'));
 console.log('firing up on port %d', app.get('port'));
+console.log(classData.main());
