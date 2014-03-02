5784d2b80379da5535d1bc159899a660d430a789
Mike Dory <mdory@kbsp.com>
2014-03-02 09:13:03 -0500

    classdata!

---
+++ classData.js
@@ -0,0 +1,15 @@
+
+/**
+ * classData.js
+ * all data required for the view
+ */
+
+// ========
+module.exports = {
+  bodyCopy: function () {
+    // whatever
+  },
+  classCopy: function () {
+    // whatever
+  }
+};
