a5f36fe8899babe71d44c70ffb8fd1cadaa34448
Mike Dory <mdory@kbsp.com>
2014-03-02 19:14:13 -0500

    spacing cleanup

--- modules/classData.js
+++ modules/classData.js
@@ -14,9 +14,7 @@ module.exports = {
       pageDescription: 'Dynamic Web Development (Server) &mdash; Spring 2014',
       sidebarTitle: 'DWD @ ITP',
       classDescription: '<p>This experimental 7-week, 2 point course will provide a framework for learning how to develop and program web applications and will focus on server side development.  Rather than the traditional model of in-class lectures, assignments, projects, and office hours, this course will heavily leverage existing online and offline resources (videos, articles, interactive tutorials, online courses, books, and more) with class time fully devoted to group and individual help sessions.</p>' +
-        
         '<p>This course will be focused on developing and deploying server side web applications using JavaScript, Node.js with the Express framework, and NoSQL databases on cloud based infrastructure.  Additional topics will include login and session management, web services and APIs, and will lightly touch on front-end web development.</p>' +
-
         '<p>Prerequisites include Comm Lab Web and Introduction to Computational Media.</p>',
       instructors: [
         {firstName: "Cory", lastName: "Forsyth", email: ''},
