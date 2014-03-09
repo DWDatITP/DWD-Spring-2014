f5e121bb97b1d4d35f6fc27af525258a143bd054
Mike Dory <mdory@kbsp.com>
2014-03-08 20:44:54 -0500

    adding support for onhashchange stuff

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,41 @@
 $( document ).ready(function() {
 
-}
+  // make sure the browser supports onhashchange
+  if (!("onhashchange" in window)) {
+    // I do not have support for IE in this,
+    // so I've kind of got nothing here. Welp.
+  }
+
+  // every time the hash changes, check the classes
+  function locationHashChanged() {
+    // console.log(newHash);
+    updateClassesByHash(location.hash);
+  }
+
+  // check that 
+  function updateClassesByHash(newHash) {
+    // check and remove old active classes
+    var classes = 'menu-item-divided pure-menu-selected';
+    $('#menu ul li').removeClass(classes);
+
+    // try to match all known 
+    if (newHash === '') {
+      $('#menu #nav-item-home').addClass(classes);
+    }
+    else if (newHash === '#instructors') {
+      $('#menu #nav-item-instructors').addClass(classes);
+    }
+    else if (newHash === '#schedule') {
+      $('#menu #nav-item-schedule').addClass(classes);
+    }
+  
+  }
+
+  if (location.hash === "#somecoolfeature") {
+      console.log(location.hash);
+  }
+
+  // on first load, and on each hash change, run the check
+  updateClassesByHash(location.hash);
+  window.onhashchange = locationHashChanged;
+});
