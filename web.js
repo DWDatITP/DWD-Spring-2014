87e3ef373b1b2d0862a9a80c0951ab28bb5c71b2
Mike Dory <mike@dory.me>
2014-03-25 23:56:35 -0400

    putting the whole config section in a configure block

--- web.js
+++ web.js
@@ -1,35 +1,33 @@
 
 /**
- * app.js
+ * web.js
  * Contains structure for the main app
  */
 
 // require the third-party node modules
-var express = require('express'),
-  hbs = require('hbs'),
-  jsonfile = require('jsonfile');
-
-// load up the class data from the JSON file
-var classDataFile = __dirname + '/data/classData.json';
-var classData = jsonfile.readFileSync(classDataFile);
+var express = require('express');
 
 // app = express, naturally
 var app = express();
 
-// tell the app where our views and static things live
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+// configure all the things
+app.configure(function(){
 
-// tell the app to rock the handlebars action
-app.set('view engine', 'html');
-app.set('layout','layout');
-app.engine('html', require('hogan-express'));
+  // tell the app where our views and static things live
+  app.set('views', __dirname + '/views');
+  app.use(express.static(__dirname + '/public'));
 
-// shiny shiny logger
-app.use(express.logger('dev'));
+  // tell the app to rock the handlebars action
+  app.set('view engine', 'html');
+  app.set('layout','layout');
+  app.engine('html', require('hogan-express'));
 
-// define env variables
-app.set('port', process.env.PORT || 3000);
+  // shiny shiny logger
+  app.use(express.logger('dev'));
+
+  // define env variables
+  app.set('port', process.env.PORT || 3000);
+});
 
 // define us some routes
 app.get('/', function(req, res) {
